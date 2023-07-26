97777703484a40c284e5998513ceb6970deef7a0
Ben <ben.marsh304@gmail.com>
2023-07-25 20:50:23 -0700

    Created new file to tune hyperparameters of model.

--- hyperparameter_tuning.py
+++ hyperparameter_tuning.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
 from helper_functions import evaluate
 from sklearn.svm import SVC
+
 
 # Import training data.
 print('Importing data...')
@@ -48,19 +49,32 @@ preprocessing = ColumnTransformer([
 
 h1n1_svc_pipeline = Pipeline([
     ('preprocessing', preprocessing),
-    ('model', SVC(C=1, gamma='auto'))
+    ('model', SVC())
 ])
 seasonal_svc_pipeline = Pipeline([
     ('preprocessing', preprocessing),
-    ('model', SVC(C=1, gamma='auto'))
+    ('model', SVC())
 ])
+
+param_grid = {'model__C': [.75, 1, 1.25],
+              'model__gamma': ['scale', 'auto', 1]
+              }
+
+# Hyperparameter tuning
+h1n1_grid = GridSearchCV(h1n1_svc_pipeline, param_grid=param_grid, verbose=10, scoring='roc_auc')
+seasonal_grid = GridSearchCV(seasonal_svc_pipeline, param_grid=param_grid, verbose=10, scoring='roc_auc')
+
 
 # Train model
 print('Training models...')
-h1n1_svc_pipeline.fit(X_train, y_train['h1n1_vaccine'])
-seasonal_svc_pipeline.fit(X_train, y_train['seasonal_vaccine'])
+h1n1_grid.fit(X_train, y_train['h1n1_vaccine'])
+seasonal_grid.fit(X_train, y_train['seasonal_vaccine'])
 
 # Evaluate model
 print('Evaluating models...')
-score_h1n1 = evaluate(h1n1_svc_pipeline, 'h1n1_vaccine', X_test, y_test)
-score_seasonal = evaluate(seasonal_svc_pipeline, 'seasonal_vaccine', X_test, y_test)
+score_h1n1 = evaluate(h1n1_grid, 'h1n1_vaccine', X_test, y_test)
+score_seasonal = evaluate(seasonal_grid, 'seasonal_vaccine', X_test, y_test)
+
+h1n1_best_params = h1n1_grid.get_params()
+seasonal_best_params = seasonal_grid.get_params()
+print(f'Best parameters for h1n1 model: {h1n1_best_params}\nBest parameters for seasonal model: {seasonal_best_params}')
